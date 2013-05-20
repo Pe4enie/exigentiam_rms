a9d1485982fb63997332863d9619d51a59957e10
Pe4enie <ne4eHue@gmail.com>
2013-05-21 00:07:00 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExigentiamRms::Application.config.secret_token = 'f535401da90157bf9e7456d504b09dc22b1425160200d75a1f5fe5b3798cf99544178a01f5a14eb3d0a9cb9631c7e2e874757a23fcee5225d6f8fe021f300521'
